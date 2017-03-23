ba79f1480e3c4580b76e17cacd99e8694b5b2842
Prateek <prateekarora5796@gmail.com>
2017-03-23 19:05:56 +0530

    abc

---
+++ app/controllers/charges_controller.rb
@@ -0,0 +1,23 @@
+cclass ChargesController < ApplicationController
+	
+	def create
+	  product = Product.find(params[:product_id])
+	  # Amount in cents
+
+	  customer = Stripe::Customer.create(
+	    :email => params[:stripeEmail],
+	    :source  => params[:stripeToken],
+	    :plan => product.sku
+	  )
+
+	  purchase = Purchase.create(email: params[:stripeEmail], card: params[:stripeToken], 
+	  	amount: product.price_in_cents, description: product.description, currency: "usd",
+	  	customer_id: customer.id, product_id: product.id, uuid: SecureRandom.uuid)
+
+	  redirect_to purchase
+
+	rescue Stripe::CardError => e
+	  flash[:error] = e.message
+	  redirect_to new_charge_path
+	end
+end
