2d11baa6827f79eafc8643280875508ecee38411
Prateek <prateekarora5796@gmail.com>
2017-03-23 19:59:28 +0530

    abc1

---
+++ db/migrate/20170323142609_adduuid_to_purchases.rb
@@ -0,0 +1,4 @@
+class AdduuidToPurchases < ActiveRecord::Migration[5.0]
+  def change
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170323132530) do
+ActiveRecord::Schema.define(version: 20170323142609) do
 
   create_table "purchases", force: :cascade do |t|
     t.string   "email"
